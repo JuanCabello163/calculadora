6bbcb04512207ea17f5023f84aab481f552b6c0f
JuanCabello163 <cabellojuan717@gmail.com>
2023-08-31 18:44:28 -0500

    primer commit

---
+++ calcula/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace calcula
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnUno = new Button();
+            cajaResultado = new TextBox();
+            lblHistorial = new Label();
+            btnDos = new Button();
+            btnTres = new Button();
+            btnSuma = new Button();
+            btnResta = new Button();
+            btnSeis = new Button();
+            btnCinco = new Button();
+            btnCuatro = new Button();
+            btnMultiplicacion = new Button();
+            btnNueve = new Button();
+            btnOcho = new Button();
+            btnSiete = new Button();
+            btnBorrar = new Button();
+            btnReset = new Button();
+            btnDivision = new Button();
+            btnModulo = new Button();
+            btnCero = new Button();
+            btnPunto = new Button();
+            btnResultado = new Button();
+            SuspendLayout();
+            // 
+            // btnUno
+            // 
+            btnUno.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnUno.Location = new Point(44, 302);
+            btnUno.Name = "btnUno";
+            btnUno.Size = new Size(52, 39);
+            btnUno.TabIndex = 0;
+            btnUno.Text = "1";
+            btnUno.UseVisualStyleBackColor = true;
+            btnUno.Click += btnUno_Click;
+            // 
+            // cajaResultado
+            // 
+            cajaResultado.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            cajaResultado.Location = new Point(44, 59);
+            cajaResultado.Name = "cajaResultado";
+            cajaResultado.ReadOnly = true;
+            cajaResultado.Size = new Size(260, 32);
+            cajaResultado.TabIndex = 0;
+            cajaResultado.Text = "0";
+            cajaResultado.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lblHistorial
+            // 
+            lblHistorial.AutoSize = true;
+            lblHistorial.Location = new Point(44, 22);
+            lblHistorial.Name = "lblHistorial";
+            lblHistorial.Size = new Size(0, 15);
+            lblHistorial.TabIndex = 2;
+            // 
+            // btnDos
+            // 
+            btnDos.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDos.Location = new Point(113, 302);
+            btnDos.Name = "btnDos";
+            btnDos.Size = new Size(52, 39);
+            btnDos.TabIndex = 3;
+            btnDos.Text = "2";
+            btnDos.UseVisualStyleBackColor = true;
+            btnDos.Click += btnDos_Click;
+            // 
+            // btnTres
+            // 
+            btnTres.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTres.Location = new Point(183, 302);
+            btnTres.Name = "btnTres";
+            btnTres.Size = new Size(52, 39);
+            btnTres.TabIndex = 4;
+            btnTres.Text = "3";
+            btnTres.UseVisualStyleBackColor = true;
+            btnTres.Click += btnTres_Click;
+            // 
+            // btnSuma
+            // 
+            btnSuma.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSuma.Location = new Point(252, 302);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(52, 39);
+            btnSuma.TabIndex = 5;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = true;
+            btnSuma.Click += btnSuma_Click;
+            // 
+            // btnResta
+            // 
+            btnResta.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnResta.Location = new Point(252, 247);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(52, 39);
+            btnResta.TabIndex = 9;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = true;
+            btnResta.Click += btnResta_Click;
+            // 
+            // btnSeis
+            // 
+            btnSeis.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSeis.Location = new Point(183, 247);
+            btnSeis.Name = "btnSeis";
+            btnSeis.Size = new Size(52, 39);
+            btnSeis.TabIndex = 8;
+            btnSeis.Text = "6";
+            btnSeis.UseVisualStyleBackColor = true;
+            btnSeis.Click += btnSeis_Click;
+            // 
+            // btnCinco
+            // 
+            btnCinco.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCinco.Location = new Point(113, 247);
+            btnCinco.Name = "btnCinco";
+            btnCinco.Size = new Size(52, 39);
+            btnCinco.TabIndex = 7;
+            btnCinco.Text = "5";
+            btnCinco.UseVisualStyleBackColor = true;
+            btnCinco.Click += btnCinco_Click;
+            // 
+            // btnCuatro
+            // 
+            btnCuatro.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCuatro.Location = new Point(44, 247);
+            btnCuatro.Name = "btnCuatro";
+            btnCuatro.Size = new Size(52, 39);
+            btnCuatro.TabIndex = 6;
+            btnCuatro.Text = "4";
+            btnCuatro.UseVisualStyleBackColor = true;
+            btnCuatro.Click += btnCuatro_Click;
+            // 
+            // btnMultiplicacion
+            // 
+            btnMultiplicacion.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMultiplicacion.Location = new Point(252, 192);
+            btnMultiplicacion.Name = "btnMultiplicacion";
+            btnMultiplicacion.Size = new Size(52, 39);
+            btnMultiplicacion.TabIndex = 13;
+            btnMultiplicacion.Text = "X";
+            btnMultiplicacion.UseVisualStyleBackColor = true;
+            btnMultiplicacion.Click += btnMultiplicacion_Click;
+            // 
+            // btnNueve
+            // 
+            btnNueve.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNueve.Location = new Point(183, 192);
+            btnNueve.Name = "btnNueve";
+            btnNueve.Size = new Size(52, 39);
+            btnNueve.TabIndex = 12;
+            btnNueve.Text = "9";
+            btnNueve.UseVisualStyleBackColor = true;
+            btnNueve.Click += btnNueve_Click;
+            // 
+            // btnOcho
+            // 
+            btnOcho.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOcho.Location = new Point(113, 192);
+            btnOcho.Name = "btnOcho";
+            btnOcho.Size = new Size(52, 39);
+            btnOcho.TabIndex = 11;
+            btnOcho.Text = "8";
+            btnOcho.UseVisualStyleBackColor = true;
+            btnOcho.Click += btnOcho_Click;
+            // 
+            // btnSiete
+            // 
+            btnSiete.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSiete.Location = new Point(44, 192);
+            btnSiete.Name = "btnSiete";
+            btnSiete.Size = new Size(52, 39);
+            btnSiete.TabIndex = 10;
+            btnSiete.Text = "7";
+            btnSiete.UseVisualStyleBackColor = true;
+            btnSiete.Click += btnSiete_Click;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnBorrar.Location = new Point(252, 136);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(52, 39);
+            btnBorrar.TabIndex = 17;
+            btnBorrar.Text = "<";
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnReset.Location = new Point(183, 136);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(52, 39);
+            btnReset.TabIndex = 16;
+            btnReset.Text = "C";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // btnDivision
+            // 
+            btnDivision.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDivision.Location = new Point(113, 136);
+            btnDivision.Name = "btnDivision";
+            btnDivision.Size = new Size(52, 39);
+            btnDivision.TabIndex = 15;
+            btnDivision.Text = "/";
+            btnDivision.UseVisualStyleBackColor = true;
+            btnDivision.Click += btnDivision_Click;
+            // 
+            // btnModulo
+            // 
+            btnModulo.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnModulo.Location = new Point(44, 136);
+            btnModulo.Name = "btnModulo";
+            btnModulo.Size = new Size(52, 39);
+            btnModulo.TabIndex = 14;
+            btnModulo.Text = "%";
+            btnModulo.UseVisualStyleBackColor = true;
+            btnModulo.Click += btnModulo_Click;
+            // 
+            // btnCero
+            // 
+            btnCero.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCero.Location = new Point(44, 357);
+            btnCero.Name = "btnCero";
+            btnCero.Size = new Size(121, 39);
+            btnCero.TabIndex = 18;
+            btnCero.Text = "0";
+            btnCero.UseVisualStyleBackColor = true;
+            btnCero.Click += btnCero_Click;
+            // 
+            // btnPunto
+            // 
+            btnPunto.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPunto.Location = new Point(183, 357);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(52, 39);
+            btnPunto.TabIndex = 19;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = true;
+            btnPunto.Click += btnPunto_Click;
+            // 
+            // btnResultado
+            // 
+            btnResultado.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnResultado.Location = new Point(252, 357);
+            btnResultado.Name = "btnResultado";
+            btnResultado.Size = new Size(52, 39);
+            btnResultado.TabIndex = 20;
+            btnResultado.Text = "=";
+            btnResultado.UseVisualStyleBackColor = true;
+            btnResultado.Click += btnResultado_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(380, 450);
+            Controls.Add(btnResultado);
+            Controls.Add(btnPunto);
+            Controls.Add(btnCero);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnReset);
+            Controls.Add(btnDivision);
+            Controls.Add(btnModulo);
+            Controls.Add(btnMultiplicacion);
+            Controls.Add(btnNueve);
+            Controls.Add(btnOcho);
+            Controls.Add(btnSiete);
+            Controls.Add(btnResta);
+            Controls.Add(btnSeis);
+            Controls.Add(btnCinco);
+            Controls.Add(btnCuatro);
+            Controls.Add(btnSuma);
+            Controls.Add(btnTres);
+            Controls.Add(btnDos);
+            Controls.Add(lblHistorial);
+            Controls.Add(cajaResultado);
+            Controls.Add(btnUno);
+            Name = "Form1";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnUno;
+        private TextBox cajaResultado;
+        private Label lblHistorial;
+        private Button btnDos;
+        private Button btnTres;
+        private Button btnSuma;
+        private Button btnResta;
+        private Button btnSeis;
+        private Button btnCinco;
+        private Button btnCuatro;
+        private Button btnMultiplicacion;
+        private Button btnNueve;
+        private Button btnOcho;
+        private Button btnSiete;
+        private Button btnBorrar;
+        private Button btnReset;
+        private Button btnDivision;
+        private Button btnModulo;
+        private Button btnCero;
+        private Button btnPunto;
+        private Button btnResultado;
+    }
+}
